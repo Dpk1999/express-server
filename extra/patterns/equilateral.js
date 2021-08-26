74ea3599a8261a54dac1f2eeb3b85a5d81846b56
Dpk1999 <deepak.gaikwad@successive.tech>
2021-08-26 15:18:13 +0530

    diamond, equilateral pattern

---
+++ extra/patterns/equilateral.js
@@ -0,0 +1,21 @@
+function createDimondShape(size){
+    for(var i=1;i<=size;i++){
+       for(var s=size-1;s>=i;s--){
+          process.stdout.write(" ");
+       }
+       for(var j=1;j<=i;j++){
+          process.stdout.write("* ")
+       }
+       console.log();
+    }
+    if(i==size+1){
+       for(var i=1;i<=size-1;i++){
+          for(var s=1;s<=i;s++){
+             process.stdout.write(" ");
+          }
+          
+       }
+    }
+  }
+  const size = process.argv.slice(2);
+  createDimondShape(Number(5));
