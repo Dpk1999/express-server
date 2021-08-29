a2b59c0915cfac83e89e8975cb4be718b889f4a5
Dpk1999 <deepak.gaikwad@successive.tech>
2021-08-29 13:32:37 +0530

    create email validation & permission.js

--- extra/utils/permissions.js
+++ extra/utils/permissions.js
@@ -7,3 +7,30 @@ permissions =
     delete: [],
     }
 }
+
+const hasPermission = (moduleName, role, permissionType) =>{
+    
+    let found = false
+    const obj = permissions[moduleName]         // fetching the module from name
+    const permission = obj[permissionType]      // fetching type of permission
+    permission.forEach(element => {     // iterating to check permission
+        if(element == role){
+                found = true        // if role is found in permission the return true
+                return found
+        }
+    });
+
+    return found            // if not found, automatically return false
+}
+
+console.log(hasPermission('getUsers','trainer','read'))
+
+/*
+
+    Sample o/p:
+
+    hasPermission('getUsers','trainer','read')
+
+    true
+
+ */
