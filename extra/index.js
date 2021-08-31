407ab6d0344c2c391c351065832bbc44f696f05d
Dpk1999 <deepak.gaikwad@successive.tech>
2021-08-31 17:54:42 +0530

    index.js

---
+++ extra/index.js
@@ -0,0 +1,15 @@
+
+import {diamond} from "./patterns/index.js";
+import {equilateral} from "./patterns/index.js";
+import {hasPermission, validateUsers} from "./utils/index.js";
+import {users} from "./constant.js";
+
+
+console.log("It is a diamond pattern")
+diamond(5)
+console.log("It is a equilateral pattern")
+equilateral(10)
+console.log("It is a boolean value True and False")
+console.log(hasPermission('getUsers','trainer','read'))
+console.log("To check the email is valid or not and it's count")
+validateUsers(users)
