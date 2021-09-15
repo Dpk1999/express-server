9ec294c1b6f9b9f737c93fa2e35058fa2c294d9c
Dpk1999 <deepak.gaikwad@successive.tech>
2021-09-15 20:51:17 +0530

    fix dist

--- dist/extraTs/utils/haspermission.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=haspermission.js.map
--- dist/src/controllers/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserRoutes = exports.TraineeRoutes = void 0;
-// import { Router } from 'express';
-const trainee_1 = require("./trainee");
-exports.TraineeRoutes = trainee_1.default;
-const user_1 = require("./user");
-exports.UserRoutes = user_1.default;
-//# sourceMappingURL=index.js.map
