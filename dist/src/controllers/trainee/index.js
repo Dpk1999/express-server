9522176d482076e01278d1961c7a9faabb402b8d
Dpk1999 <deepak.gaikwad@successive.tech>
2021-09-15 19:59:33 +0530

    remove dist

--- dist/src/controllers/trainee/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const routes_1 = require("./routes");
-exports.default = routes_1.default;
-//# sourceMappingURL=index.js.map
--- dist/src/routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const controllers_1 = require("./controllers");
-const router = (0, express_1.Router)();
-router.use('/trainee', controllers_1.default);
-exports.default = router;
-//# sourceMappingURL=routes.js.map
