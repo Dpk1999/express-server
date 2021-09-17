45c908d7825a483d1639d95941796ae128ad1b71
Dpk1999 <deepak.gaikwad@successive.tech>
2021-09-17 16:17:40 +0530

    add function disconnect mongodb

--- src/libs/Database.ts
+++ src/libs/Database.ts
@@ -14,4 +14,10 @@ export default class Database{
 
         })
     }
+    public static disconnect() {
+        mangoose.disconnect((err) => {
+          if (!err) console.log('Disconnected from MongoDB.');
+        });
+      
+    }
 }
