1c71fc4e4c57b4f7f9772211793f55041501a397
Dpk1999 <deepak.gaikwad@successive.tech>
2021-09-22 18:08:45 +0530

    some changes

--- extraTs/utils/permissions.ts
+++ extraTs/utils/permissions.ts
@@ -1,17 +1,16 @@
 import { permissions } from "../constants"
-import { GetUsers } from "../interface";
-
-const hasPermission = (moduleName: string, role: string, permissionType: string): boolean => {
 
 
-  const obj: GetUsers = permissions[moduleName];
-  const permission: string[] = obj[permissionType];
-  if (!obj || !permission) {
+const hasPermission = (moduleName: string, role: string, permissionType: string)=> {
+
+
+  const obj: any  = permissions[moduleName];
+  if (!obj || !obj[permissionType]) {
     console.log(`${role} do not have permission to ${permissionType} permissions for the module ${moduleName}`);
     return false;
   }
 
-  if (permission.includes(role)) {
+  if (obj[permissionType].includes(role)) {
     console.log(`${role} do not have permission to ${permissionType} permissions for the module ${moduleName}`);
     return false;
   }
